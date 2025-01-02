f4ffd2b9bb5116603b57e550d5322d59cfa63ea2
Marco Andriola <152104290+zaless92@users.noreply.github.com>
2025-01-02 14:45:47 +0100

    Comparison Operators

---
+++ freeCodeCampCourse/2.Fundamentals/ComparisonOperators.py
@@ -0,0 +1,8 @@
+# operatori di comparazione
+a = 3
+b = 5
+
+a == b # "a" uguale a "b" -> False
+a != b # "a" diverso da "b" -> True
+a > b # "a" maggiore di "b" -> False
+a <= b # "a" minore o uguale di "b" -> True
